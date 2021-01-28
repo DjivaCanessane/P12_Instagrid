228133c623cf315d705a83680bb31af974292298
Canessane Djiveradjane <djiva.canessane@icloud.com>
2021-01-28 15:23:31 +0100

    custom fonts added in info.plist and in the contentView

--- instagridSwiftUI/ContentView.swift
+++ instagridSwiftUI/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Instagrid")
+                .font(Font.custom("ThirstySoftRegular", size: 30))
+                .padding()
+            Text("Instagrid")
+                .padding()
+        }
     }
 }
 
