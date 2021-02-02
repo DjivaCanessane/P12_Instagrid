e777367e6139204f75a4a4da3e0424b89a399a96
Canessane Djiveradjane <djiva.canessane@icloud.com>
2021-02-02 17:10:32 +0100

    rectangles in GridLayout are well spaced

--- instagridSwiftUI/GridLayout.swift
+++ instagridSwiftUI/GridLayout.swift
@@ -10,20 +10,27 @@ import SwiftUI
 struct GridLayout: View {
     var body: some View {
         ZStack {
-            Rectangle()
-                .fill(SwiftUI.Color.blue)
+
             VStack {
                 HStack {
-                    
+                    Rectangle().fill(SwiftUI.Color.red)
+                    Rectangle().fill(SwiftUI.Color.green)
                 }
+                .padding(.horizontal, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                .padding(.top, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
                 HStack {
-                    
+                    Rectangle().fill(SwiftUI.Color.purple)
+                    Rectangle().fill(SwiftUI.Color.orange)
                 }
+                .padding(.horizontal, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                .padding(.bottom, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
             }
+            
         }
         .background(SwiftUI.Color.blue)
         .aspectRatio(1, contentMode: .fill)
         .frame(width: 280, height: 280, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
