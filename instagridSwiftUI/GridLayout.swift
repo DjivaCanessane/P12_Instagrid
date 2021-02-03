7f54a9dea6c50d39801a0e4255c7f7d1afbb2a59
Canessane Djiveradjane <djiva.canessane@icloud.com>
2021-02-03 16:11:41 +0100

    Replaced rectangles with buttons in gridLayout

--- instagridSwiftUI/GridLayout.swift
+++ instagridSwiftUI/GridLayout.swift
@@ -8,32 +8,96 @@
 import SwiftUI
 
 struct GridLayout: View {
+    @State var showTopLeftRectangle = true
+    @State var showBottomRightRectangle = true
+
     var body: some View {
         ZStack {
 
             VStack {
-                HStack {
-                    Rectangle().fill(SwiftUI.Color.red)
-                    Rectangle().fill(SwiftUI.Color.green)
+                VStack {
+                    HStack {
+                        if showTopLeftRectangle {
+                            Button(action: {
+                                print("Top left button was tapped")
+                            }) {
+                                ZStack {
+                                    Rectangle().fill(SwiftUI.Color.white)
+                                    Image("Plus")
+                                }
+                            }
+                        }
+                        Button(action: {
+                            print("Top right button was tapped")
+                        }) {
+                            ZStack {
+                                Rectangle().fill(SwiftUI.Color.white)
+                                Image("Plus")
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 8)
+                    .padding(.top, 8)
+                    HStack {
+                        Button(action: {
+                            print("Bottom left button was tapped")
+                        }) {
+                            ZStack {
+                                Rectangle().fill(SwiftUI.Color.white)
+                                Image("Plus")
+                            }
+                        }
+                        if showBottomRightRectangle {
+                            Button(action: {
+                                print("Bottom right button was tapped")
+                            }) {
+                                ZStack {
+                                    Rectangle().fill(SwiftUI.Color.white)
+                                    Image("Plus")
+                                }
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 8)
+                    .padding(.bottom, 8)
+                    
+                    
+                    }
+                .background(SwiftUI.Color.blue)
+                .aspectRatio(1, contentMode: .fill)
+                .frame(width: 280, height: 280, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                Button("ToggleUP") {
+                    withAnimation{
+                        self.showTopLeftRectangle.toggle()
+                    }
                 }
-                .padding(.horizontal, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                .padding(.top, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                HStack {
-                    Rectangle().fill(SwiftUI.Color.purple)
-                    Rectangle().fill(SwiftUI.Color.orange)
+                Button("ToggleBottom") {
+                    withAnimation{
+                        self.showBottomRightRectangle.toggle()
+                    }
                 }
-                .padding(.horizontal, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                .padding(.bottom, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
             }
             
+            
         }
-        .background(SwiftUI.Color.blue)
-        .aspectRatio(1, contentMode: .fill)
-        .frame(width: 280, height: 280, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        
         
     }
+    
+    
 }
 
+extension View {
+    @ViewBuilder func isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
+        if hidden {
+            if !remove {
+                self.hidden()
+            }
+        } else {
+            self
+        }
+    }
+}
 struct MainLayout_Previews: PreviewProvider {
     static var previews: some View {
         GridLayout()
