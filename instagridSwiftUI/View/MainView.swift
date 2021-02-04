9ed8e1475f791a008c8568dc0c682e5dc094191a
Canessane Djiveradjane <djiva.canessane@icloud.com>
2021-02-04 15:33:47 +0100

    swipe gesture is added to gridLayout, for both modes

--- instagridSwiftUI/View/MainView.swift
+++ instagridSwiftUI/View/MainView.swift
@@ -12,14 +12,35 @@ struct MainView: View {
     @Environment(\.verticalSizeClass) var verticalSizeClass: UserInterfaceSizeClass?
     @Environment(\.horizontalSizeClass) var horizontalSizeClass: UserInterfaceSizeClass?
     
+    private func isPortraitMode() -> Bool {
+        return horizontalSizeClass == .compact && verticalSizeClass == .regular
+    }
+    
     var body: some View {
         let topItems = TopItems()
         let gridLayout = GridLayout()
+            .gesture(DragGesture(minimumDistance: 3, coordinateSpace: .local)
+                .onEnded({ (value) in
+                    if value.translation.width < 0 {
+                        // left swipe
+                        if !isPortraitMode() {
+                            print("left swipe")
+                        }
+                    } else if value.translation.height < 0 {
+                        // up swipe
+                        if isPortraitMode() {
+                            print("up swipe")
+                        }
+                        
+                    }
+                })
+            )
+            
         let layoutButtons = LayoutButtons()
         ZStack {
             Color.yellow
                 .edgesIgnoringSafeArea(.all)
-            if horizontalSizeClass == .compact && verticalSizeClass == .regular {
+            if isPortraitMode() {
                 GeometryReader { geometry in
                     VStack {
                         topItems
