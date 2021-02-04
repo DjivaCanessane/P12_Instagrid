bfa0bf7a53a54e6a96b991d3a532bc006afcde8d
Canessane Djiveradjane <djiva.canessane@icloud.com>
2021-02-04 15:14:05 +0100

    Responsive design: works as well in portrait mode as landscape mode

--- instagridSwiftUI/View/MainView.swift
+++ instagridSwiftUI/View/MainView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct MainView: View {
     @EnvironmentObject var layoutViewModel: LayoutViewModel
+    @Environment(\.verticalSizeClass) var verticalSizeClass: UserInterfaceSizeClass?
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass: UserInterfaceSizeClass?
     
     var body: some View {
         let topItems = TopItems()
@@ -17,16 +19,33 @@ struct MainView: View {
         ZStack {
             Color.yellow
                 .edgesIgnoringSafeArea(.all)
-            GeometryReader { geometry in
-                VStack {
-                    topItems
-                    gridLayout
-                    layoutButtons
-                    
-                }.frame(width: geometry.size.width,
-                        height: geometry.size.height,
+            if horizontalSizeClass == .compact && verticalSizeClass == .regular {
+                GeometryReader { geometry in
+                    VStack {
+                        topItems
+                        gridLayout
+                        layoutButtons
+                        
+                    }.frame(width: geometry.size.width,
+                            height: geometry.size.height,
+                            alignment: .center)
+                }
+            } else {
+                GeometryReader { geometry in
+                    HStack {
+                        VStack {
+                            topItems
+                            layoutButtons
+                            
+                        }
+                        gridLayout
+                    }
+                    .frame(width: geometry.size.width,
+                            height: geometry.size.height,
                         alignment: .center)
+                }
             }
+            
             
 
         }
@@ -37,6 +56,6 @@ struct MainView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
+        MainView().environmentObject(LayoutViewModel())
     }
 }
--- instagridSwiftUI/View/Supporting views/TopItems.swift
+++ instagridSwiftUI/View/Supporting views/TopItems.swift
@@ -8,19 +8,37 @@
 import SwiftUI
 
 struct TopItems: View {
+    @Environment(\.verticalSizeClass) var verticalSizeClass: UserInterfaceSizeClass?
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass: UserInterfaceSizeClass?
+    
     var body: some View {
         VStack {
-            Text("Instagrid")
-                .font(Font.custom("ThirstySoftRegular", size: 30))
-                .padding()
-            Image("Arrow Up")
-                .resizable()
-                .aspectRatio(1, contentMode: .fill)
-                .frame(width: 15, height: 15, alignment: .center)
-            Text("Swipe up to share")
-                .font(Font.custom("Delm-Medium", size: 20))
-                .padding()
+            if horizontalSizeClass == .compact && verticalSizeClass == .regular {
+                Text("Instagrid")
+                    .font(Font.custom("ThirstySoftRegular", size: 30))
+                    .padding()
+                Image("Arrow Up")
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fill)
+                    .frame(width: 15, height: 15, alignment: .center)
+                Text("Swipe up to share")
+                    .font(Font.custom("Delm-Medium", size: 20))
+                    .padding()
 
+            } else {
+                Text("Instagrid")
+                    .font(Font.custom("ThirstySoftRegular", size: 30))
+                    .padding()
+                Image("Arrow Left")
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fill)
+                    .frame(width: 15, height: 15, alignment: .center)
+                Text("Swipe left to share")
+                    .font(Font.custom("Delm-Medium", size: 20))
+                    .padding()
+
+            }
+            
         }
     }
 }
